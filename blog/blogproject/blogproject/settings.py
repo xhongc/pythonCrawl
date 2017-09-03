3729dcfbded0add22a6f929fd9255cb43372a285
408737515@qq.com <408737515@qq.com>
2017-09-03 16:18:10 +0800

    个人网站建设 Signed-off-by: 408737515@qq.com <408737515@qq.com>

--- blog/blogproject/blogproject/settings.py
+++ blog/blogproject/blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&7f@9%gnwoxe9-8v^sqtd0nq3w6h$a@0a%+dxlt34dwek@_#_c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','.xhongc.cc']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
