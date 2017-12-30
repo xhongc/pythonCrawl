1ca6f9bccf28e6b2346c0d5567d751a72c60d089
408737515@qq.com <408737515@qq.com>
2017-12-30 18:40:15 +0800

    Signed-off-by: 408737515@qq.com <408737515@qq.com>

--- tesseract.py
+++ tesseract.py
@@ -2,34 +2,35 @@ from PIL import Image
 import pytesseract
 import os
 
-im =Image.open('captcha.jpg')
-im = im.convert('RGBA')
-pix = im.load()
-#print(pix[2,2])
-for x in range(im.size[0]):
-    for z in range(10):
-        pix[x,z] =(255,255,255,255)
-        pix[x, im.size[1] - z-1]=(255,255,255,255)
-for y in range(im.size[1]):
-    for z in range(10):
-        pix[z,y]=pix[im.size[0]-1-z,y]=(255,255,255,255)
-for y in range(im.size[1]):  # 二值化处理，这个阈值为R=95，G=95，B=95
-    for x in range(im.size[0]):
-        if pix[x, y][0] < 95 or pix[x, y][1] < 95 or pix[x, y][2] < 95:
-            pix[x, y] = (0, 0, 0,255)
-        else:
-            pix[x, y] = (255, 255, 255,255)
-im = im.convert('RGB')
-#print(im.size)
-im=im.resize((200,100))
-im.save('111.jpg')
-result = Image.open('111.jpg')
-result = pytesseract.image_to_string(result)
+im =Image.open('920.jpg')
+# im = im.convert('RGBA')
+# pix = im.load()
+# #print(pix[2,2])
+# for x in range(im.size[0]):
+#     for z in range(10):
+#         pix[x,z] =(255,255,255,255)
+#         pix[x, im.size[1] - z-1]=(255,255,255,255)
+# for y in range(im.size[1]):
+#     for z in range(10):
+#         pix[z,y]=pix[im.size[0]-1-z,y]=(255,255,255,255)
+# for y in range(im.size[1]):  # 二值化处理，这个阈值为R=95，G=95，B=95
+#     for x in range(im.size[0]):
+#         if pix[x, y][0] < 95 or pix[x, y][1] < 95 or pix[x, y][2] < 95:
+#             pix[x, y] = (0, 0, 0,255)
+#         else:
+#             pix[x, y] = (255, 255, 255,255)
+# im = im.convert('RGB')
+# #print(im.size)
+# im=im.resize((200,100))
+# im.save('111.jpg')
+# result = Image.open('111.jpg')
+result = pytesseract.image_to_string(im)
 result= result.replace('Z','2')
-if len(result) == 4:
-    print(result)
-else:
-    print('wrong:%s'%result)
+print(result)
+# if len(result) == 4:
+#     print(result)
+# else:
+#     print('wrong:%s'%result)
 '''import pytesseract
 from PIL import Image  # 需要加载PIL库
 import os
