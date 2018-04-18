6478e23ffb9a4d70bd3dfae8001c9915364d8be8
408737515@qq.com <408737515@qq.com>
2018-04-18 20:41:58 +0800

    Signed-off-by: 408737515@qq.com <408737515@qq.com>

--- saffe/py_smtp.py
+++ saffe/py_smtp.py
@@ -2,16 +2,16 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
-
+import time
 # 第三方 SMTP 服务
 mail_host = "smtp.qq.com"  # 设置服务器
 mail_user = "408737515@qq.com"  # 用户名
 mail_pass = "uppsptidntgmbgeg"  # 口令,QQ邮箱是输入授权码，在qq邮箱设置 里用验证过的手机发送短信获得，不含空格
 
 sender = '408737515@qq.com'
-receivers = ['2324369231@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+receivers = ['1840597778@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
-message = MIMEText('why', 'plain', 'utf-8')
+message = MIMEText('you are big pig', 'plain', 'utf-8')
 #　message['From'] = Header("408737515", 'utf-8')
 # message['To'] = Header("you", 'utf-8')
 
@@ -21,9 +21,12 @@ message['Subject'] = Header(subject, 'utf-8')
 try:
     smtpObj = smtplib.SMTP_SSL(mail_host, 465)
     smtpObj.login(mail_user, mail_pass)
-    smtpObj.sendmail(sender, receivers, message.as_string())
+    for _ in range(2):
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        print(u"邮件发送成功")
+        time.sleep(5)
     smtpObj.quit()
-    print(u"邮件发送成功")
+
 except smtplib.SMTPException as e:
     print(e)
 
