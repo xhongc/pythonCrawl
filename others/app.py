bcfe58ebe5507beb09a1fc1d66674be0497bdcfd
chao123456789 <408737515@qq.com>
2018-08-31 16:01:15 +0800

    Signed-off-by: chao123456789 <408737515@qq.com>

--- others/app.py
+++ others/app.py
@@ -1,47 +1,21 @@
-class Graphic:
-    def render(self):
-        raise NotImplementedError("You should implement this.")
+import requests
+import json
 
+session = requests.session()
+url = 'http://127.0.0.1:8080/login/'
+data = {
+    'username': 'chong',
+    'password': 'adminadmin'
+}
+html = session.post(url=url, data=data)
+html = json.loads(html.text)
+token = html['token']
+adr_url = 'http://127.0.0.1:8080/address/'
+headers = {
+    'Authorization': 'jwt ' + token
+}
+data = {"province": "11111", "city": "2", "district": "3", "address": "", "signer_name": "",
+        "add_time": "2018-08-31 15:19", "signer_mobile": ""}
 
-class CompositeGraphic(Graphic):
-    def __init__(self):
-        self.graphics = []
-
-    def render(self):
-        for graphic in self.graphics:
-            graphic.render()
-
-    def add(self, graphic):
-        self.graphics.append(graphic)
-
-    def remove(self, graphic):
-        self.graphics.remove(graphic)
-
-
-class Ellipse(Graphic):
-    def __init__(self, name):
-        self.name = name
-
-    def render(self):
-        print("Ellipse: {}".format(self.name))
-
-
-if __name__ == '__main__':
-    ellipse1 = Ellipse("1")
-    ellipse2 = Ellipse("2")
-    ellipse3 = Ellipse("3")
-    ellipse4 = Ellipse("4")
-
-    graphic1 = CompositeGraphic()
-    graphic2 = CompositeGraphic()
-
-    graphic1.add(ellipse1)
-    graphic1.add(ellipse2)
-    graphic1.add(ellipse3)
-    graphic2.add(ellipse4)
-    graphic = CompositeGraphic()
-
-    graphic.add(graphic1)
-    graphic.add(graphic2)
-
-    graphic.render()
+html = session.post(url=adr_url, headers=headers,data=data)
+print(html.text)
