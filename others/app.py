af729b250fffc2a383c33c12dff26285ea537d51
chao <408737515@qq.com>
2019-03-03 23:11:21 +0800

    dj-channels Signed-off-by: chao <408737515@qq.com>

--- others/app.py
+++ others/app.py
@@ -1,3 +1,26 @@
-a ='111'
-b='1111'
-print(b in a)
+import inspect
+
+
+class APP(object):
+    def case1(self):
+        print('case1')
+
+    def case2(self):
+        return 'asd'
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        print(type(self))
+        return '{}'.format(class_name)
+
+
+def case1():
+    print('aa')
+    return 'haha'
+
+
+# pro = [globals()[name] for name in globals() if name.startswith('case')]
+# pro[0]()
+# pro = [func for name, func in inspect.getmembers(APP, inspect.isfunction)]
+# print(pro)
+print(APP())
